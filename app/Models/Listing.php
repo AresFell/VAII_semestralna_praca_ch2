f0021a2087c7704b41faa4214f95359fd1130b76
AresFell <sarapapsova@gmail.com>
2023-12-02 14:46:23 +0100

    VAII

---
+++ app/Models/Listing.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Models;
+
+class Listing {
+    public static function all() {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Listing one',
+                'description' => 'lorem ipsum'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing two',
+                'description' => 'lorem ipsum 2'
+            ]
+        ];
+    }
+
+    public static function find($id) {
+        $listings = self::all();
+
+        foreach ($listings as $listing) {
+            if($listing['id'] == $id)
+                return $listing;
+        }
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Babičkine recepty</title>
+    <link rel="icon" type="image/png" href="resources/images/cooking-icon.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+                <img class="page-icon" src="resources/images/cooking-icon.png" alt="babičkine recepty" >
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="" class="nav-link px-2 link-body-emphasis">Všetky Recepty</a></li>
+                <li><a href="" class="nav-link px-2 link-body-emphasis">Prihlasovanie</a></li>
+            </ul>
+
+            <form class="d-flex" role="search">
+                <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</header>
+
+<div class="b-example-divider"></div>
+
+<main class="container">
+    <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+        <div class="col-lg-6 px-0">
+            <h1 class="display-4 fst-italic">Veľmi dôležitá informácia, ktorá hovorí o novinkách vo svete varenia</h1>
+            <p class="lead my-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda blanditiis consequatur cumque dolore ducimus ea eaque eligendi exercitationem hic id iure non, perspiciatis quae quibusdam sapiente similique sunt suscipit voluptate, voluptatibus. Cumque, eligendi facilis in maiores mollitia nam, odit optio quaerat qui quod quos sapiente sed sint sit veritatis.</p>
+            <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold">Pokracovanie nie je >.</a></p>
+        </div>
+    </div>
+
+    <div class="row mb-2">
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-primary-emphasis">Varenie</strong>
+                    <h3 class="mb-0">Novinka na našej stránke</h3>
+                    <div class="mb-1 text-body-secondary">Nov 19 </div>
+                    <p class="card-text mb-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, et!</p>
+                    <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                        Continue reading
+                        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                    </a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                    <img src="resources/images/blog-photos/blog_photo_cooking.jpg" class="blog-picture" alt="dokoncoavanie varenia">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-success-emphasis">Pečenie</strong>
+                    <h3 class="mb-0">Preco su tieto cookies najlepsie</h3>
+                    <div class="mb-1 text-body-secondary">Nov 17</div>
+                    <p class="mb-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, voluptatem.</p>
+                    <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                        Continue reading
+                        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                    </a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                    <img src="resources/images/blog-photos/stawberry_jam_cookies.jpg" class="blog-picture" alt="strawberry jam cookies">
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<div class="b-example-divider"></div>
+
+<div class="container">
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <div class="col mb-3">
+            <p class="text-body-secondary">© 2023</p>
+        </div>
+
+        <!--<div class="col mb-3"></div>-->
+
+        <div class="col mb-3">
+            <h5>Obchod</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">O projekte</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Inzercia</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Osobne udaje a sukromie</a></li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+            <h5>Pomoc</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Kontakt</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Napíšte nám</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">GDPR</a></li>
+            </ul>
+        </div>
+
+    </footer>
+</div>
+
+</body>
+</html>
